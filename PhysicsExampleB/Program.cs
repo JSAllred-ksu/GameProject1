200fab3459e80da8ef0fe536261d9ae95c92f852
Nathan Bean <nhbean@WIN2.CS.KSU.EDU>
2023-08-31 22:21:53 -0500

    Updated for Visual Studio 2022

--- PhysicsExampleB/Program.cs
+++ PhysicsExampleB/Program.cs
@@ -1,14 +1,2 @@
-﻿using System;
-
-namespace PhysicsExampleB
-{
-    public static class Program
-    {
-        [STAThread]
-        static void Main()
-        {
-            using (var game = new PhysicsExampleBGame())
-                game.Run();
-        }
-    }
-}
+﻿using var game = new PhysicsExampleB.PhysicsExampleBGame();
+game.Run();
